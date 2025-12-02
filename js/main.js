67091a30deaed2c42612ffa8a6aec087b8890c39
Nataly <kolodina_melbyk@mail.ru>
2025-12-02 10:21:50 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,36 @@
-table1.onclick = function(e) {
-  if(e.target.tagName != 'TH') return
-  let th = e.target
-  sortTable(th.cellIndex, th.dataset.type, 'table1')
-}
+table1.onclick = function (e) {
+  if (e.target.tagName != "TH") return;
+  let th = e.target;
+  sortTable(th.cellIndex, th.dataset.type, "table1");
+};
 
-table2.onclick = function(e) {
-  if(e.target.tagName != "TH") return
-  let th = e.target
-  sortTable(th.cellIndex, th.dataset.type, 'table12')
-}
+table2.onclick = function (e) {
+  if (e.target.tagName != "TH") return;
+  let th = e.target;
+  sortTable(th.cellIndex, th.dataset.type, "table12");
+};
 
 function sortTable(colNum, type, id) {
-  let elem = document.getElementById(id)
-  let tbody = elem.querySelector('tbody')
-  let rowsArray = Array.from(tbody.rows)
-  let compare
-  switch(type) {
-    case 'number':
-      compare = function(rowA, rowB) {
-        return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML
-      }
-      break
-      case 'string':
-      compare = function(rowA, rowB) {
-        return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1 : -1
-      }
-      break
+  let elem = document.getElementById(id);
+  let tbody = elem.querySelector("tbody");
+  let rowsArray = Array.from(tbody.rows);
+  let compare;
+  switch (type) {
+    case "number":
+      compare = function (rowA, rowB) {
+        return rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML;
+      };
+      break;
+    case "string":
+      compare = function (rowA, rowB) {
+        return rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML
+          ? 1
+          : -1;
+      };
+      break;
   }
-  rowsArray.sort(compare)
-  tbody.append(...rowsArray)
+  rowsArray.sort(compare);
+  tbody.append(...rowsArray);
 }
 
 if (!localStorage.getItem("goods")) {
@@ -40,9 +42,9 @@ let myModal = new bootstrap.Modal(document.getElementById("exampleModal"), {
 });
 
 let options = {
-    valueNames: ['name', 'price']
-}
-let userList
+  valueNames: ["name", "price"],
+};
+let userList;
 
 document
   .querySelector("button.add-new")
@@ -124,7 +126,7 @@ function update_goods() {
         );
       }
     }
-    userList = new List('goods', options);
+    userList = new List("goods", options);
   } else {
     table1.hidden = true;
     table2.hidden = true;
@@ -132,85 +134,80 @@ function update_goods() {
   document.querySelector(".price-result").innerHTML = result_price + "&#8381;";
 }
 
-document.querySelector('.list').addEventListener('click', function(e) {
-    if(!e.target.dataset.delete) {
-        return 
-    }
-    Swal.fire({
-        title:'Внимание!',
-        text: 'Вы действительно хотите удалить товар?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Да',
-        cancelButtonText: 'Отмена',
-    }).then((result) => {
-      if(result.isConfirmed) {
-        let goods = JSON.parse(localStorage.getItem('goods'))
-        for (let i = 0; i < goods.length; i++) {
-          if(goods[i][0] == e.target.dataset.delete) {
-            goods.splice(i, 1)
-            localStorage.setItem('goods', JSON.stringify(goods))
-            update_goods()
-          }
-          
+document.querySelector(".list").addEventListener("click", function (e) {
+  if (!e.target.dataset.delete) {
+    return;
+  }
+  Swal.fire({
+    title: "Внимание!",
+    text: "Вы действительно хотите удалить товар?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Да",
+    cancelButtonText: "Отмена",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      let goods = JSON.parse(localStorage.getItem("goods"));
+      for (let i = 0; i < goods.length; i++) {
+        if (goods[i][0] == e.target.dataset.delete) {
+          goods.splice(i, 1);
+          localStorage.setItem("goods", JSON.stringify(goods));
+          update_goods();
         }
-        Swal.fire(
-          "Удалено!",
-          "Выбранный товар был успешно удален",
-          "success"
-        )
       }
-    })
-})
+      Swal.fire("Удалено!", "Выбранный товар был успешно удален", "success");
+    }
+  });
+});
 
-document.querySelector('.list').addEventListener('click', function(e) {
-    if(!e.target.dataset.goods) {
-        return 
+document.querySelector(".list").addEventListener("click", function (e) {
+  if (!e.target.dataset.goods) {
+    return;
+  }
+  let goods = JSON.parse(localStorage.getItem("goods"));
+  for (let i = 0; i < goods.length; i++) {
+    if (goods[i][3] > 0 && goods[i][0] == e.target.dataset.goods) {
+      goods[i].splice(3, 1, goods[i][3] - 1);
+      goods[i].splice(4, 1, goods[i][4] + 1);
+      localStorage.setItem("goods", JSON.stringify(goods));
+      update_goods();
     }
-let goods = JSON.parse(localStorage.getItem('goods'))
- for (let i = 0; i < goods.length; i++) {
-  if(goods[i][3]>0 && goods[i][0] == e.target.dataset.goods) {
-    goods[i].splice(3,1, goods[i][3]-1)
-    goods[i].splice(4,1, goods[i][4]+1)
-    localStorage.setItem('goods', JSON.stringify(goods))
-    update_goods()
-    }
- }
-  })
+  }
+});
 
-  document.querySelector('.cart').addEventListener('click', function(e) {
-    if(!e.target.dataset.delete) {
-        return 
+document.querySelector(".cart").addEventListener("click", function (e) {
+  if (!e.target.dataset.delete) {
+    return;
+  }
+  let goods = JSON.parse(localStorage.getItem("goods"));
+  for (let i = 0; i < goods.length; i++) {
+    if (goods[i][4] > 0 && goods[i][0] == e.target.dataset.delete) {
+      goods[i].splice(3, 1, goods[i][3] + 1);
+      goods[i].splice(4, 1, goods[i][4] - 1);
+      localStorage.setItem("goods", JSON.stringify(goods));
+      update_goods();
     }
-let goods = JSON.parse(localStorage.getItem('goods'))
- for (let i = 0; i < goods.length; i++) {
-  if(goods[i][4]>0 && goods[i][0] == e.target.dataset.delete) {
-    goods[i].splice(3, 1, goods[i][3]+1)
-    goods[i].splice(4, 1, goods[i][4]-1)
-    localStorage.setItem('goods', JSON.stringify(goods))
-    update_goods()
-    }
- }
-  })
+  }
+});
 
-    document.querySelector('.cart').addEventListener('input', function(e) {
-    if(!e.target.dataset.goodid) {
-        return 
+document.querySelector(".cart").addEventListener("input", function (e) {
+  if (!e.target.dataset.goodid) {
+    return;
+  }
+  let goods = JSON.parse(localStorage.getItem("goods"));
+  for (let i = 0; i < goods.length; i++) {
+    if (goods[i][0] == e.target.dataset.goodid) {
+      goods[i][5] = e.target.value;
+      goods[i][6] =
+        goods[i][4] * goods[i][2] -
+        goods[i][4] * goods[i][2] * goods[i][5] * 0.01;
+      localStorage.setItem("goods", JSON.stringify(goods));
+      update_goods();
+      let input = document.querySelector(`[data-goodid="${goods[i][0]}"]`);
+      input.focus();
+      input.selectionStart = input.value.length;
     }
-    let goods = JSON.parse(localStorage.getItem('goods'))
-     for (let i = 0; i < goods.length; i++) {
-      if(goods[i][0] == e.target.dataset.goodid) {
-        goods[i][5] = e.target.value
-        goods[i][6] =
-          goods[i][4] * goods[i][2] -
-          goods[i][4] * goods[i][2] * goods[i][5] * 0.01
-           localStorage.setItem('goods', JSON.stringify(goods))
-           update_goods()
-           let input = document.querySelector(`[data-goodid="${goods[i][0]}"]`)
-           input.focus()
-           input.selectionStart = input.value.length
-      }
-     }
-    })
+  }
+});
